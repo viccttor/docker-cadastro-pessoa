9a8782d84fd446809fa9807d2a45cbfe14f0a961
Victor Henrique <vhsdm7@gmail.com>
2023-02-01 10:50:07 -0300

    Finalizado scripts

---
+++ README.md
@@ -0,0 +1,4 @@
+Docker: Utilização prática no cenário de Microsserviços
+
+Cadastro de uma pessoa.
+ 
---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dados (
+    idPessoa int,
+    Nome varchar(50),
+    cpf varchar(14),
+    Endereco varchar(150), 
+    Cidade varchar(50),
+    Host varchar(50)
+);
